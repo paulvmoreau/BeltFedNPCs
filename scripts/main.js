6b786cdc2bc894f91db4dc167adc1c65201a2b20
Derrick <dh@dadi.co>
2016-04-22 12:42:12 -0500

    Move loading logic to main, normalize

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,45 @@
-var generator = new NPCGenerator();
+var generator = null;
+var additionalData = {};
+load();
+
+function getJSON(url, cb){
+  var xhttp = new XMLHttpRequest();
+  xhttp.overrideMimeType('application/json');
+  xhttp.open('GET', url, true);
+  xhttp.onreadystatechange = function () {
+    if (xhttp.readyState == 4 && xhttp.status == '200') {
+      // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+      var json = null;
+      try {
+        json = JSON.parse(xhttp.responseText);
+      } catch(e) {
+        console.log('Error getting remote JSON', e);
+        return cb(null, e);
+      }
+      return cb(json, null);
+    }
+  };
+  xhttp.send(null);
+}
+
+function loadAdditionalData(cb){
+  getJSON('data/backgrounds.json', function(backgrounds, err){
+    additionalData.backgrounds = backgrounds.backgrounds;
+    getJSON('data/names.json', function(names, err){
+      additionalData.names = names;
+      return cb(true, false);
+    });
+  });
+}
+
+function load(){
+  var genBtn = document.getElementById('generateCharacterBtn');
+  genBtn.disabled = true;
+  loadAdditionalData(function(){
+    generator = new NPCGenerator(additionalData);
+    genBtn.disabled = false;
+  });
+}
 
 function generateCharacter(){
   var data = generator.generate();
